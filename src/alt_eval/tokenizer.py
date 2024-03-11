ce73322a39773488e57f6b151f4d22f0fcc83bd2
Ondřej Cífka <cifkao@users.noreply.github.com>
2024-03-11 09:23:25 -0700

    Add support for Japanese and basic support for languages without spaces

--- src/alt_eval/tokenizer.py
+++ src/alt_eval/tokenizer.py
@@ -1,5 +1,6 @@
 import copy
 from dataclasses import dataclass, field
+import functools
 import unicodedata
 
 import regex as re
@@ -59,6 +60,30 @@ def tokens_as_words(tokens: list[Token]) -> list[Token]:
     return result
 
 
+# fmt: off
+UNICODE_SCRIPTS = [
+    "Adlm", "Aghb", "Ahom", "Arab", "Armi", "Armn", "Avst", "Bali", "Bamu", "Bass", "Batk", "Beng",
+    "Bhks", "Bopo", "Brah", "Brai", "Bugi", "Buhd", "Cakm", "Cans", "Cari", "Cham", "Cher", "Chrs",
+    "Copt", "Cpmn", "Cprt", "Cyrl", "Deva", "Diak", "Dogr", "Dsrt", "Dupl", "Egyp", "Elba", "Elym",
+    "Ethi", "Geor", "Glag", "Gong", "Gonm", "Goth", "Gran", "Grek", "Gujr", "Guru", "Hang", "Hani",
+    "Hano", "Hatr", "Hebr", "Hira", "Hluw", "Hmng", "Hmnp", "Hung", "Ital", "Java", "Kali", "Kana",
+    "Kawi", "Khar", "Khmr", "Khoj", "Kits", "Knda", "Kthi", "Lana", "Laoo", "Latn", "Lepc", "Limb",
+    "Lina", "Linb", "Lisu", "Lyci", "Lydi", "Mahj", "Maka", "Mand", "Mani", "Marc", "Medf", "Mend",
+    "Merc", "Mero", "Mlym", "Modi", "Mong", "Mroo", "Mtei", "Mult", "Mymr", "Nagm", "Nand", "Narb",
+    "Nbat", "Newa", "Nkoo", "Nshu", "Ogam", "Olck", "Orkh", "Orya", "Osge", "Osma", "Ougr", "Palm",
+    "Pauc", "Perm", "Phag", "Phli", "Phlp", "Phnx", "Plrd", "Prti", "Rjng", "Rohg", "Runr", "Samr",
+    "Sarb", "Saur", "Sgnw", "Shaw", "Shrd", "Sidd", "Sind", "Sinh", "Sogd", "Sogo", "Sora", "Soyo",
+    "Sund", "Sylo", "Syrc", "Tagb", "Takr", "Tale", "Talu", "Taml", "Tang", "Tavt", "Telu", "Tfng",
+    "Tglg", "Thaa", "Thai", "Tibt", "Tirh", "Tnsa", "Toto", "Ugar", "Vaii", "Vith", "Wara", "Wcho",
+    "Xpeo", "Xsux", "Yezi", "Yiii", "Zanb",
+]
+UNICODE_SCRIPTS_NO_SPACES = [
+    "Egyp", "Hani", "Hira", "Hluw", "Lina", "Linb", "Xsux", "Kana", "Khmr", "Laoo", "Mymr", "Phag",
+    "Lana", "Thai", "Tibt",
+]
+# fmt: on
+
+
 class LyricsTokenizer:
     """A Moses-based tokenizer for lyrics.
 
@@ -80,14 +105,42 @@ class LyricsTokenizer:
             r"(?P<a>)(?P<b>'s)\b|\b(?P<a>wie|für)(?P<b>'n)\b", flags=re.IGNORECASE
         )
 
+        # A regex to match the boundary between two letters from two different scripts, or between a
+        # number and a letter from a script that does not use spaces between words.
+        self._different_scripts_re = re.compile(
+            r"|".join(
+                [rf"(?<=[\p{{L}}&&\p{{{s}}}])(?=[\p{{L}}--\p{{{s}}}])" for s in UNICODE_SCRIPTS]
+                + [rf"(?<=[\p{{L}}&&\p{{{s}}}])(?=[0-9])" for s in UNICODE_SCRIPTS_NO_SPACES]
+                + [rf"(?<=[0-9])(?=[\p{{L}}&&\p{{{s}}}])" for s in UNICODE_SCRIPTS_NO_SPACES]
+            ),
+            flags=re.VERSION1,
+        )
+
+        # A regex to match a character in a script that does not use spaces between words.
+        self._no_spaces_re = re.compile(
+            r"(" + r"|".join([rf"\p{{{s}}}" for s in UNICODE_SCRIPTS_NO_SPACES]) + r")",
+            flags=re.VERSION1,
+        )
+
+    @functools.cached_property
+    def _fugashi_tagger(self):
+        try:
+            import fugashi
+
+            return fugashi.Tagger()
+        except (ImportError, RuntimeError) as e:
+            raise RuntimeError(
+                "Failed to initialize the tagger for Japanese. Please make sure to install the "
+                "required dependencies via `pip install 'jam-alt[ja]'."
+            ) from e
+
     def __call__(self, text: str, language: str = "en") -> list[Token]:
         """
         Tokenize the given text.
 
         Args:
             text: A string to tokenize.
-            language: A language code supported by `sacremoses`: either an ISO 639-1 language code,
-                or "cjk" for Chinese, Japanese and Korean.
+            language: An ISO 639-1 language code.
 
         Returns:
             A list of `Token` objects.
@@ -146,6 +199,17 @@ class LyricsTokenizer:
                 if language == "de":
                     # Split contractions
                     line = self._contraction_de_re.sub(r"\g<a> \g<b>", line)
+
+                if language == "ja":
+                    # Tokenize Japanese
+                    line = " ".join(w.surface.strip() for w in self._fugashi_tagger(line))
+                else:
+                    # In other languages that do not use spaces to separate words, treat each
+                    # character as a separate word
+                    text = self._no_spaces_re.sub(r" \1 ", text)
+
+                # Insert spaces between characters from different scripts
+                text = self._different_scripts_re.sub(" ", text)
 
                 result.extend(line.strip().split())
 
